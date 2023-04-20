2835a2d01a78044ed97f660abbf18c9855c02f45
yogadharsha98 <yyogakrishanthi@gmail.com>
2023-04-20 09:20:45 +0530

    ADD: redux for cars

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './components/Header';
 import Home
@@ -8,7 +7,7 @@ import Home
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
+      <Header />
       <Home />
       
     </div>
